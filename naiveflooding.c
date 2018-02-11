e9506f6ea95f8a5dcf9d0b9ba32a18b8b34c7415
Rishabh Jain <rj2315@imperial.ac.uk>
2018-02-11 19:42:56 +0000

    Using netflood now. Lot better results.

--- naiveflooding.c
+++ naiveflooding.c
@@ -8,7 +8,7 @@
 #include "contiki.h"
 #include "net/rime/rime.h"
 #include "random.h"
-
+#include "net/rime/netflood.h"
 #include "dev/leds.h"
 #include "dev/serial-line.h"
 
@@ -27,7 +27,7 @@ struct message{
 };
 
 struct message payload_send,payload_recv;
-static struct broadcast_conn broadcast;
+static struct netflood_conn netflood;
 static struct ctimer message_sent;
 /*---------------------------------------------------------------------------*/
 PROCESS(main_process, "Naive Flooding");
@@ -44,7 +44,7 @@ request_message(void *ptr){
 
 /*---------------------------------------------------------------------------*/
 static void
-broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
+netflood_recv(struct netflood_conn *c, const linkaddr_t *from)
 {
   bool received = false;
 
@@ -52,42 +52,42 @@ broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 
   printf("Broadcast recv from %d of %d with id %d\n", from->u8[0],payload_recv.nodes[0],payload_recv.flood_id);
 
-  int i = 0;
-  for(i = 0; i < payload_recv.node_count;i++){
-    if(payload_recv.nodes[i] == linkaddr_node_addr.u8[0]){
-      received = true;
-      break;
-    }
-  }
-
-  if(payload_recv.node_count > (NO_OF_NODES-1)){
-      received = true;
-  }
-
-  if(!received){
-    payload_recv.nodes[payload_recv.node_count]  = linkaddr_node_addr.u8[0];
-    payload_recv.node_count = payload_recv.node_count + 1;
-
-    packetbuf_copyfrom(&payload_recv,sizeof(payload_recv));
-    broadcast_send(&broadcast);
-    printf("Broadcast message sent\n");
-    if(linkaddr_node_addr.u8[0] == 1){
-      ctimer_reset(&message_sent);
-    }
-  }
+  // int i = 0;
+  // for(i = 0; i < payload_recv.node_count;i++){
+  //   if(payload_recv.nodes[i] == linkaddr_node_addr.u8[0]){
+  //     received = true;
+  //     break;
+  //   }
+  // }
+  //
+  // if(payload_recv.node_count > 3){
+  //     received = true;
+  // }
+  //
+  // if(!received){
+  //   payload_recv.nodes[payload_recv.node_count]  = linkaddr_node_addr.u8[0];
+  //   payload_recv.node_count = payload_recv.node_count + 1;
+  //
+  //   packetbuf_copyfrom(&payload_recv,sizeof(payload_recv));
+  //   netflood_send(&netflood, payload_recv.flood_id);
+  //   printf("Broadcast message sent\n");
+  //   if(linkaddr_node_addr.u8[0] == 1){
+  //     ctimer_reset(&message_sent);
+  //   }
+  // }
 }
 
-static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+static const struct netflood_callbacks netflood_call = {netflood_recv};
 
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(main_process, ev, data)
 {
-  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+  PROCESS_EXITHANDLER(netflood_close(&netflood);)
   char *buf;
 
   PROCESS_BEGIN();
-  powertrace_start(CLOCK_SECOND * POWERTRACE_INTERVAL);
-  broadcast_open(&broadcast, 129, &broadcast_call);
+  // powertrace_start(CLOCK_SECOND * POWERTRACE_INTERVAL);
+  netflood_open(&netflood, 2, 129, &netflood_call);
   if(linkaddr_node_addr.u8[0] == 1){
     ctimer_set(&message_sent, CLOCK_SECOND * 10,request_message,NULL);
   }
@@ -101,7 +101,7 @@ PROCESS_THREAD(main_process, ev, data)
       payload_send.nodes[0] = linkaddr_node_addr.u8[0];
       payload_send.node_count = 1;
       packetbuf_copyfrom(&payload_send, sizeof(payload_recv));
-      broadcast_send(&broadcast);
+      netflood_send(&netflood, atoi(buf));
       printf("Broadcast message sent %d\n",atoi(buf));
 
       if(linkaddr_node_addr.u8[0] == 1){
