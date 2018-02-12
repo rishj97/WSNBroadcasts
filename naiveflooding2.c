2b362696acd5aa89f11088301f434691a98f0b20
Rishabh Jain <rj2315@imperial.ac.uk>
2018-02-12 18:50:33 +0000

    changed back to part 1 because why not.

--- naiveflooding2.c
+++ naiveflooding2.c
@@ -8,6 +8,7 @@
 #include "contiki.h"
 #include "net/rime/rime.h"
 #include "random.h"
+#include "mynetflood.h"
 #include "net/netstack.h"
 #include "dev/leds.h"
 #include "dev/serial-line.h"
@@ -16,7 +17,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <powertrace.h>
-#define NO_OF_NODES 15
+#define NO_OF_NODES 30
 // power trace interval in seconds
 #define POWERTRACE_INTERVAL 10
 
@@ -29,7 +30,7 @@ struct message{
 };
 
 struct message payload_send,payload_recv;
-static struct broadcast_conn broadcast;
+static struct netflood_conn netflood;
 static struct ctimer message_sent;
 static struct ctimer turn_mac_on;
 /*---------------------------------------------------------------------------*/
@@ -51,54 +52,28 @@ mac_on(void *ptr){
 }
 /*---------------------------------------------------------------------------*/
 static void
-broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
+netflood_recv(struct netflood_conn *c, const linkaddr_t *from, const linkaddr_t *originator, uint8_t seqno, uint8_t hops)
 {
   bool received = false;
 
   packetbuf_copyto(&payload_recv);
 
   printf("Broadcast recv from %d of %d with id %d\n", from->u8[0],payload_recv.nodes[0],payload_recv.flood_id);
-
-  int i = 0;
-  for(i = 0; i < payload_recv.node_count;i++){
-    if(payload_recv.nodes[i] == linkaddr_node_addr.u8[0]){
-      received = true;
-      break;
-    }
-  }
-
-  if(payload_recv.node_count > (NO_OF_NODES-1)){
-      received = true;
-  }
-
-  if(!received){
-    payload_recv.nodes[payload_recv.node_count]  = linkaddr_node_addr.u8[0];
-    payload_recv.node_count = payload_recv.node_count + 1;
-
-    packetbuf_copyfrom(&payload_recv,sizeof(payload_recv));
-    broadcast_send(&broadcast);
-
-    printf("Broadcast message sent\n");
-    if(linkaddr_node_addr.u8[0] == 1){
-      ctimer_reset(&message_sent);
-    }
-  } else {
-    NETSTACK_MAC.off(0);
-    ctimer_set(&turn_mac_on, CLOCK_SECOND * RADIO_OFF_INTERVAL, mac_on, NULL);
-  }
+  NETSTACK_MAC.off(0);
+  ctimer_set(&turn_mac_on, CLOCK_SECOND * RADIO_OFF_INTERVAL, mac_on, NULL);
 }
 
-static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+static const struct netflood_callbacks netflood_call = {netflood_recv};
 
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(main_process, ev, data)
 {
-  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+  PROCESS_EXITHANDLER(netflood_close(&netflood);)
   char *buf;
 
   PROCESS_BEGIN();
   powertrace_start(CLOCK_SECOND * POWERTRACE_INTERVAL);
-  broadcast_open(&broadcast, 129, &broadcast_call);
+  netflood_open(&netflood, 2, 129, &netflood_call);
   if(linkaddr_node_addr.u8[0] == 1){
     ctimer_set(&message_sent, CLOCK_SECOND * 10,request_message,NULL);
   }
@@ -113,13 +88,11 @@ PROCESS_THREAD(main_process, ev, data)
       payload_send.node_count = 1;
       packetbuf_copyfrom(&payload_send, sizeof(payload_recv));
       NETSTACK_MAC.on();
-      broadcast_send(&broadcast);
+      netflood_send(&netflood, atoi(buf));
       printf("Broadcast message sent %d\n",atoi(buf));
-
       if(linkaddr_node_addr.u8[0] == 1){
         ctimer_restart(&message_sent);
       }
-
     }
 
   PROCESS_END();
